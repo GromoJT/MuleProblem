66f05a9c0106dd027787b7d47e59067ad88e5bfc
GromoJT <85227567+GromoJT@users.noreply.github.com>
2022-03-21 13:32:58 +0100

    Update szukaj.py

--- szukaj.py
+++ szukaj.py
@@ -1,9 +1,14 @@
 import json
 import random
-import math
 
-udzwig = 150.0
-gabaryt = 150.0
+
+
+
+
+hmSize = 15
+i=0
+HM = []
+HMV = []
 
 ids = []
 nam = []
@@ -12,12 +17,7 @@ siz = []
 val = []
 ris = []
 
-ids2 = []
-nam2 = []
-wei2 = []
-siz2 = []
-val2 = []
-ris2 = []
+
 
 with open('wynik.json') as f:
     data = json.load(f)
@@ -37,49 +37,146 @@ for item in data['items']:
 #print(ris)
 #print(len(ids))
 
-dalej=True
-ilosc_prob = 10000
 
-k_udz = 0.0
-k_gab = 0.0
-k_val = 0.0
-k_ryz = 0.0
-fin = 0.0
 
-while dalej:
-    r = random.randint(0, len(ids)-1)
-    if r in ids2:
-        pass
-    if(udzwig > wei[r]) and (gabaryt > siz[r]):
-        udzwig = udzwig-wei[r]
-        gabaryt = gabaryt-siz[r]
+
+
+
+
+while i < hmSize:
+    #print("TEST")
+    udzwig = 150.0
+    gabaryt = 150.0
+
+    k_udz = 0.0
+    k_gab = 0.0
+    k_val = 0.0
+    k_ryz = 0.0
+    fin = 0.0
     
-        ids2.append(ids[r])
-        nam2.append(nam[r])
-        wei2.append(wei[r])
-        siz2.append(siz[r])
-        val2.append(val[r])
-        ris2.append(ris[r])
+    dalej=True
+    ilosc_prob = 100
+
+    ids2 = []
+    nam2 = []
+    wei2 = []
+    siz2 = []
+    val2 = []
+    ris2 = []
+    
+    while dalej:
+        r = random.randint(0, len(ids)-1)
+        if r in ids2:
+            pass
+        if(udzwig > wei[r]) and (gabaryt > siz[r]):
+            udzwig = udzwig-wei[r]
+            gabaryt = gabaryt-siz[r]
         
-        k_udz = k_udz + wei[r]
-        k_gab = k_gab + siz[r]
-        k_val = k_val + val[r]
-        k_ryz = k_ryz + ris[r]
+            ids2.append(ids[r])
+            nam2.append(nam[r])
+            wei2.append(wei[r])
+            siz2.append(siz[r])
+            val2.append(val[r])
+            ris2.append(ris[r])
+            
+            k_udz = k_udz + wei[r]
+            k_gab = k_gab + siz[r]
+            k_val = k_val + val[r]
+            k_ryz = k_ryz + ris[r]
+            
+            fin = k_val / k_ryz
+            
+            
+            
+            
+            
+        else:
+            ilosc_prob = ilosc_prob - 1
+            
+            if ilosc_prob < 1:
+                dalej = False
         
-    else:
-        ilosc_prob = ilosc_prob - 1
+    
+    i = i+1
+    
+    HM.append(ids2)
+    HMV.append(fin)
+
+
+
+#print(HM)
+print(HMV)
+
+koniec=False
+ilosc_prob_2 = 5000000
+
+while koniec!=True:
+    
+    
+    udzwig = 150.0
+    gabaryt = 150.0
+
+    k_udz = 0.0
+    k_gab = 0.0
+    k_val = 0.0
+    k_ryz = 0.0
+    fin = 0.0
+
+    dalej=True
+    ilosc_prob = 100
+
+    ids2 = []
+    nam2 = []
+    wei2 = []
+    siz2 = []
+    val2 = []
+    ris2 = []
+
+    while dalej:
+        r = random.randint(0, len(ids)-1)
+        if r in ids2:
+            pass
+        if(udzwig > wei[r]) and (gabaryt > siz[r]):
+            udzwig = udzwig-wei[r]
+            gabaryt = gabaryt-siz[r]
         
-        if ilosc_prob < 1:
-            dalej = False
+            ids2.append(ids[r])
+            nam2.append(nam[r])
+            wei2.append(wei[r])
+            siz2.append(siz[r])
+            val2.append(val[r])
+            ris2.append(ris[r])
+            
+            k_udz = k_udz + wei[r]
+            k_gab = k_gab + siz[r]
+            k_val = k_val + val[r]
+            k_ryz = k_ryz + ris[r]
+            
+            fin = k_val / k_ryz
+            
+            
+        else:
+            ilosc_prob = ilosc_prob - 1
+            
+            if ilosc_prob < 1:
+                dalej = False
 
+    o=0
+    while o < hmSize:
+       if(fin>HMV[o]):
+           HMV[o]=fin
+           HM[o]=ids2
+           #print("zmiana!")
+           #print(ilosc_prob_2)
+           break
+       else:
+           ilosc_prob_2=ilosc_prob_2-1
+           #print("pudlo!")
+           
+       o=o+1
+    if ilosc_prob_2<=0:
+        koniec = True
+#print(HM)
+print(HMV)
+        
 
-
-
-fin = k_val / k_ryz
-print(k_udz)
-print(k_gab)
-print(k_val)
-print(k_ryz)
-print(fin)
-
-print(ids2)
